aaafe7a9da95a6e5d203b9b01c05a4786bd86d78
inamumu <>
2022-05-17 22:01:22 +0900

    数字の位置が正しいときは緑にする

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,7 @@ class _PuzzlePageState extends State<PuzzlePage> {
               child: Center(
                 child: TilesView(
                   numbers: tileNumbers,
+                  isCorrect: calcIsCorrect(tileNumbers),
                 ),
               ),
             ),
@@ -106,13 +107,25 @@ class _PuzzlePageState extends State<PuzzlePage> {
       ),
     );
   }
+
+  bool calcIsCorrect(List<int> numbers) {
+    final correctNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 0];
+    for (int i = 0; i < correctNumbers.length; i++) {
+      if (numbers[i] != correctNumbers[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 class TilesView extends StatelessWidget {
   final List<int> numbers;
+  final bool isCorrect;
   const TilesView({
     Key? key,
     required this.numbers,
+    required this.isCorrect,
   }) : super(key: key);
 
   @override
@@ -129,7 +142,7 @@ class TilesView extends StatelessWidget {
         }
         return TileView(
           number: number,
-          color: Colors.blue,
+          color: isCorrect ? Colors.green : Colors.blue,
           onPressed: () => {},
         );
       }).toList(),
